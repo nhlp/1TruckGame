9512e0850c53c7a7b1355ba4857e2085590491bc
nhlp <39793478+nhlp@users.noreply.github.com>
2021-03-06 15:20:44 +0300

    Add files via upload

---
+++ FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
